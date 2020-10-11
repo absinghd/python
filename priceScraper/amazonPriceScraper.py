521b06f54577b2ba0bf95d87ddb1f8a69ef16c51
Bicky Singh <9771641+absinghd@users.noreply.github.com>
2020-10-11 03:12:57 -0700

    scraper working

--- priceScraper/amazonPriceScraper.py
+++ priceScraper/amazonPriceScraper.py
@@ -1,9 +1,32 @@
-from urllib.request import urlopen
-url = input('what product price do you want to track? ')
+import asyncio
+from pyppeteer import launch
 
-page = urlopen(url)
+url = input("which link? ")
 
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+urlName = url.split('/')
+productName = urlName[3]
 
-print(html)
+async def main():
+    try:
+        browser = await launch()
+        page = await browser.newPage()
+        await page.goto(url)
+  #  await page.screenshot({'path': 'example.png'})
+        await page.waitForSelector("#priceblock_ourprice")
+        element = await page.querySelector('#priceblock_ourprice')
+        price = await page.evaluate('(element) => element.textContent', element)
+        #element2 = await page.querySelector("#productTitle")
+        #title = await page.evaluate('(element) => element.textContent', element2)
+
+    
+    # print(dimensions)
+    # >>> {'width': 800, 'height': 600, 'deviceScaleFactor': 1}
+        await browser.close()
+        #print(title)
+        print(productName)
+        print(price)
+
+    except:
+        print('Error')
+
+asyncio.get_event_loop().run_until_complete(main())
